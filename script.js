4f9c550c030e7d468d4f3fb70bd473104539aaa5
Aaron <iaaron.xyz@outlook.com>
2022-08-22 03:16:26 -0500

    works: Play a round vs the computer

---
+++ script.js
@@ -0,0 +1,85 @@
+// Options
+const choices = ["rock", "paper", "scissors"];
+let record = {"user": 0, "computer": 0};
+
+
+// Ask the user choice
+function getUserChoice() {
+    // Ask and get the user choice
+    let choice = prompt("Rock, paper or scissors?").toLowerCase();
+    // Check user option exist
+    if (choices.includes(choice)) {
+        return choice;
+    }
+    else {
+        console.error("You must choose rock, paper or scissors!");
+    }
+}
+// Get computer choice
+function getComputerChoice() {
+    // Choose randomly a value among those options
+    return chooseArrayItem(choices)
+}
+// Play and choose the winner of the round
+function playRound(playerChoice, computerChoice) {
+    // User Choose rock
+    if (playerChoice == "rock") {
+        if (computerChoice == "rock") {
+            return "Tie"
+        }
+        else if (computerChoice == "paper") {
+            record.computer += 1;
+            return `${computerChoice} beats ${playerChoice}. Computer wins this round!`
+        }
+        else {
+            record.user += 1;
+            return `${playerChoice} beats ${computerChoice}. You win this round!`
+        }
+    }
+    // User Choose PAPER
+    if (playerChoice == "paper") {
+        if (computerChoice == "rock") {
+            return `${playerChoice} beats ${computerChoice}. You win this round!`
+        }
+        else if (computerChoice == "paper") {
+            record.computer += 1;
+            return "Tie"
+        }
+        else {
+            record.user += 1;
+            return `${computerChoice} beats ${playerChoice}. Computer wins this round!`
+        }
+    }
+    // User Choose SCISSORS
+    if (playerChoice == "scissors") {
+        if (computerChoice == "rock") {
+            record.computer += 1;
+            return `${computerChoice} beats ${playerChoice}. Computer wins this round!`
+        }
+        else if (computerChoice == "paper") {
+            record.user += 1;
+            return `${playerChoice} beats ${computerChoice}. You win this round!`
+        }
+        else {
+            record.user += 1;
+            return "Tie"
+        }
+    }
+}
+// Set the game to 5 rounds, Best 3 of 5 wins
+// Annoounce the winner of the game
+
+
+// HELPERS
+// Choose an array element randomly
+function chooseArrayItem(arr) {
+    return arr[Math.floor(Math.random()*arr.length)]
+}
+
+
+// Main Code
+let user = getUserChoice();
+let computer = getComputerChoice(); 
+round = playRound(user, computer);
+console.log(round);
+console.log(record);
